9519bc36402be4f85b71c9cc57d198734ff3aec1
suraj-7 <suraj5106@yahoo.com>
2020-06-29 18:32:24 +0530

    Push and POP added

--- stack.go
+++ stack.go
@@ -5,7 +5,7 @@ import (
 )
 
 type stack struct {
-	st [100]int
+	data [100]int
 	top int 
 }
 
@@ -13,6 +13,62 @@ func New() *stack {
 	return &stack{ top : -1}
 }
 
+type stackOverflow struct {}
+
+func (so stackOverflow) Error() string {
+	return "Stack is full!"
+}
+
+type stackEmpty struct {}
+
+func (se stackEmpty) Error() string {
+	return "stack is empty!"
+}
+
+func (s *stack) push(elem int) error {
+   if s.top == 99 {
+	   return stackOverflow{}
+   }
+   s.top++
+   s.data[s.top] = elem
+   return nil
+}
+
+func (s *stack) pop() (int,error) {
+	if s.top == -1 {
+		return -1,stackEmpty{}
+	}
+	elem := s.data[s.top]
+    s.top--
+	return elem,nil
+}
+
+func (s *stack) peep() (int,error) {
+	if s.top == -1 {
+		return -1,stackEmpty{}
+	}
+	return s.data[s.top],nil 
+}
+
 func main() {
-	s := stack{}
+	s := New()
+	err := s.push(5)
+	if err != nil {
+		fmt.Println("Error Occured : ",err)
+		return
+	}
+	fmt.Println("5 is pushed")
+	val,err := s.pop() 
+    if err != nil {
+		fmt.Println("Error Occured : ",err)
+		return
+	}
+	fmt.Println("Poped Element : ",val)
+	val,err = s.peep()
+	if err != nil {
+		fmt.Println("Error Occured : ",err)
+		return
+	}
+	fmt.Println("Element at top : ",val)
+	
 }
