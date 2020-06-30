fb2ddc3680b111ac3b2647e2e62d4443cc01cc1c
suraj-7 <suraj5106@yahoo.com>
2020-06-30 12:33:39 +0530

    updated test cases

--- stack_test.go
+++ stack_test.go
@@ -2,40 +2,36 @@ package main
 
 import "testing"
 
-func (s *stack)TestPush(t *testing.T) {
-	for i:=0;i<100;i++ {
-		err := s.push(i)
-		if err != nil {
-			t.Errorf("Expected no error , but got %v",err)
-			return
+func TestPush(t *testing.T) {
+	tests := []struct {
+		input  int
+	}{
+		{0},{1},{2},{3},
+	}
+    s:= New()
+	for _, tc := range tests {
+		r := s.push(tc.input)
+		if r != nil {
+			t.Fatalf("Expected no error , but got %v",r)
 		}
 	}
 	
 }
 
-func (s *stack)TestPop(t *testing.T) {
-	for i:=0;i<100;i++ {
-		output:= 99-i
-		val,err := s.pop()
-		if err != nil {
-			t.Errorf("Expected no error , but got %v",err)
-			return
-		}
+func TestPop(t *testing.T) {
 	
-		if val != output {
-			t.Errorf("expected output to be %v , but got %v",output,val)
-			return
+		s:= New()
+		_,err := s.pop()
+		if err == nil {
+			t.Errorf("Expected Stack is empty , but got %v",err)
 		}
-	}
-	
-
 }
 
-func (s *stack)TestPeep(t *testing.T) {
+func TestPeep(t *testing.T) {
+	    s:= New()
 		_,err := s.peep()
-		if err != nil {
-			t.Errorf("Expected no error , but got %v",err)
-			return
+		if err == nil {
+			t.Errorf("Expected Stack is empty , but got %v",err)
 		}
 	
 }
